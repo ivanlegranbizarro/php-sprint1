a923531b65dc5e281d37e6b17ccc8edb5ef5fa88
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-03-06 12:52:31 +0100

    Tema2 Nivel1 Ejercicio6

---
+++ Tema2/Nivel1/Ejercicio6/index.php
@@ -0,0 +1,12 @@
+<?php
+/* - Exercici 6
+Charlie em va mossegar el dit! Charlie et mossegarà el dit exactament el 50% del temps.
+
+Escriu La funció isBitten () que retorna TRUE amb un 50% de probabilitat i FALSE en cas contrari. */
+
+function isBitten()
+{
+  return rand(0, 1) == 0 ? '¡Ese perro del demonio me ha mordido!' : 'Este perro es un santo incapaz de morder a nadie!';
+}
+
+echo isBitten();
