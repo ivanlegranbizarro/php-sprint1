8a639850b7a9d43d52935380db7e9c7020fe8b9d
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-03-07 10:33:07 +0100

    Planteamiento Nivel2 Ejercicio2

---
+++ Tema2/Nivel2/Ejercicio2/index.php
@@ -0,0 +1,22 @@
+<?php
+/* - Exercici 2
+Imagina que som a una botiga on es ven:
+
+Xocolata: 1 euro
+Xiclets: 0,50 euros
+Caramels: 1,50 euros
+Implementa un programa que permeti afegir càlculs a un total de compres d'aquest tipus. Per exemple, que si comprem:
+
+2 xocolates, 1 de xiclets i 1 de caramels, tinguem un programa que permeti anar afegint els subtotals a un total, tal que així:
+
+funció(2 xocolates) + funció(1 de xiclets) + funció(1 de carmels) = 2 + 0.5 + 1.5
+
+Sent, per tant, el total, 4. */
+
+function xocolata($numXocolates)
+{
+}
+
+function calculadoraBotigaPocaVarietatLaVeritat()
+{
+}
