69a63e35d3fcc00e05a788363a695712485c7dae
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-03-11 12:11:34 +0100

    plantea ejercicio1

---
+++ Tema4/Nivel2/Ejercicio1/index.php
@@ -0,0 +1,13 @@
+<?php
+/* - Exercici 1
+Crea la classe PokerDice. Les cares d'un dau de pòquer tenen les següents figures: As, K, Q, J, 7 i 8.
+
+Crea el mètode throw que no fa altra cosa que tirar el dau, és a dir, genera un valor aleatori per a l'objecte a què se li aplica el mètode.
+
+Crea també el mètode shapeName, que digui quina és la figura que ha sortit en l'última tirada del dau en qüestió.
+
+Realitza una aplicació que permeti tirar cinc daus de pòquer alhora.
+
+A més, programa el mètode getTotalThrows que ha de mostrar el nombre total de tirades entre tots els daus. */
+
+declare(strict_types=1);
