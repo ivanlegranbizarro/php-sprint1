9ef625730a02cb9a7cbfce874d7552bafa3814c2
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-03-13 10:43:45 +0100

    planteamiento Nivel2

---
+++ Tema5/Nivel2/Ejercicio1/index.php
@@ -0,0 +1,4 @@
+<?php
+/* Escriu un programa que defineixi una classe Shape amb un constructor que rebi com a paràmetres l'ample i alt. Defineix dues subclasses; Triangle i Rectangle que heretin de Shape i que calculin respectivament l'àrea de la forma area(). */
+
+declare(strict_types=1);
