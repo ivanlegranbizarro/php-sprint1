c4f8c7a579f6417836f30188fdececfd6f18c907
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-03-13 11:11:10 +0100

    Tema5 Nivel2

--- Tema5/Nivel2/Ejercicio1/index.php
+++ Tema5/Nivel2/Ejercicio1/index.php
@@ -2,3 +2,31 @@
 /* Escriu un programa que defineixi una classe Shape amb un constructor que rebi com a paràmetres l'ample i alt. Defineix dues subclasses; Triangle i Rectangle que heretin de Shape i que calculin respectivament l'àrea de la forma area(). */
 
 declare(strict_types=1);
+
+class Shape
+{
+  public function __construct(protected int|float $ample, protected int|float $alt)
+  {
+  }
+
+  public function calculArea(): int|float
+  {
+    return 0;
+  }
+}
+
+class Triangle extends Shape
+{
+  public function calculArea(): int|float
+  {
+    return ($this->ample * $this->alt) / 2;
+  }
+}
+
+class Rectangle extends Shape
+{
+  public function calculArea(): int|float
+  {
+    return $this->ample * $this->alt;
+  }
+}
