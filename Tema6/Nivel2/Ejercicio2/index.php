e803452cf6f566762e422af679654947f4a2838c
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-03-15 09:41:02 +0100

    Tema6 Nivel2 Ejercicio2

---
+++ Tema6/Nivel2/Ejercicio2/index.php
@@ -0,0 +1,25 @@
+<?php
+/* - Exercici 2
+Sobreescriu alguna de les lògiques d’entre tots els mètodes màgics que hi ha (que no sigui __construct) */
+
+class SuperGuerrer
+{
+  public function __construct(private string $nom, private int $nivell)
+  {
+  }
+
+  public function __toString()
+  {
+    return "El nom del guerrer és $this->nom. Pot transformar-se en Superguerrer de nivell $this->nivell";
+  }
+}
+
+
+$guerrer1 = new SuperGuerrer('Goku', 4);
+echo $guerrer1;
+
+$guerrer2 = new SuperGuerrer('Vegeta', 4);
+echo $guerrer2;
+
+$guerrer3 = new SuperGuerrer('Trunks', 2);
+echo $guerrer3;
