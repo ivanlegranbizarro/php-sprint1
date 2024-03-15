7df790c9bcd2cb6369f1e6a9d39a244011bdd24b
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-03-15 10:11:37 +0100

    setup Nivel3 Ejercicio2

---
+++ Tema6/Nivel3/Ejercicio2/index.php
@@ -0,0 +1,3 @@
+<?php
+/* - Exercici 2
+Implementa una classe Car que tingui informació sobre un cotxe (marca, matrícula, tipus de combustible, velocitat màxima). A més, implementa un Trait anomenat Turbo que tingui un mètode boost() que mostri un missatge “S’ha iniciat el turbo”. Usa aquest mètode des de la classe Car. */
