130012c5bb55812860df7489d404a80f5bf2e9b7
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-03-15 09:57:04 +0100

    setup Nivel3

---
+++ Tema6/Nivel3/Ejercicio1/index.php
@@ -0,0 +1,3 @@
+<?php
+/* - Exercici 1
+Crea una classe que representi un recurs didàctic d’aquesta especialitat. Els recursos hauran de tenir un nom, un tema (que només podrà ser PHP, CSS, HTML, SQL, Laravel) un URL i un tipus de recurs (Fitxer, Article web, Vídeo). Implementa tant el tema com el tipus de recurs amb enums. */
