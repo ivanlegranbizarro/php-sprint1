449aab7ad46ca43b7e1bb39bb8b155a6b042eee8
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-03-17 09:44:43 +0100

    test de prueba

---
+++ Testing/tests/unit/PruebaTest.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+final class PruebaTest extends TestCase
+{
+  public function testPrueba(): void
+  {
+    $this->assertTrue(true);
+  }
+}
