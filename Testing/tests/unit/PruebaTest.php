ef1d4a2d2317a8e62c236dedb76b8576132472f8
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-03-17 09:48:44 +0100

    añadir atributos

--- Testing/tests/unit/PruebaTest.php
+++ Testing/tests/unit/PruebaTest.php
@@ -2,10 +2,14 @@
 
 declare(strict_types=1);
 
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\Attributes\TestDox;
 use PHPUnit\Framework\TestCase;
 
 final class PruebaTest extends TestCase
 {
+  #[Test]
+  #[TestDox('Prueba para verificar el funcionamiento de phpunit')]
   public function testPrueba(): void
   {
     $this->assertTrue(true);
