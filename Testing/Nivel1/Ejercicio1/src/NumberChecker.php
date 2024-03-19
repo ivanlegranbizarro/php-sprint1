19e02e8058ef1ad6ba13bf6c389ff00136dd3408
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-03-19 18:14:56 +0100

    end first test

--- Testing/Nivel1/Ejercicio1/src/NumberChecker.php
+++ Testing/Nivel1/Ejercicio1/src/NumberChecker.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+namespace Ejercicio1;
+
 class NumberChecker
 {
 
--- Testing/Nivel1/Ejercicio1/tests/unit/NumberCheckerTest.php
+++ Testing/Nivel1/Ejercicio1/tests/unit/NumberCheckerTest.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use Ejercicio1\NumberChecker;
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
 
