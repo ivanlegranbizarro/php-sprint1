1b239785a64804b201697c5a8bf7bd38d0279bcd
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-03-19 17:59:19 +0100

    updating gitignore and first testing

--- Testing/Nivel1/Ejercicio1/tests/unit/numberCheckerTest.php
+++ Testing/Nivel1/Ejercicio1/tests/unit/numberCheckerTest.php
@@ -2,8 +2,15 @@
 
 declare(strict_types=1);
 
+use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
 
 final class NumberCheckerTest extends TestCase
 {
+  #[Test]
+  public function probar_pares()
+  {
+    $prueba = new NumberChecker(2);
+    $this->assertTrue($prueba->isEven());
+  }
 }
