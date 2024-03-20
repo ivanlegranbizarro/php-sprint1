2e5f4bc733038a79a43f28d82dd5c0f11f274aef
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-03-20 16:42:21 +0100

    test añadir libros

--- Testing/Nivel3/Ejercicio1/src/Biblioteca.php
+++ Testing/Nivel3/Ejercicio1/src/Biblioteca.php
@@ -1,4 +1,17 @@
 <?php
+/*Necessitem crear un petit software per a tractament d’informació en una biblioteca. Per això necessitem representar la informació d’un llibre, que té:
+
+Un títol
+Un autor/autora
+Un ISBN
+Un gènere, que pot ser: Aventures, Ciència-ficció, Conte, Novel·la Policial, Paranormal, Distopia, Fantàstic.
+núm. de pàgines.
+
+Necessitem emmagatzemar el conjunt de llibres i tenir mètodes que:
+
+Afegeixin, esborrin i modifiquin un llibre de la llibreria.
+Permetin consultar llibres per títol, gènere, ISBN o autor.
+Retornar llibres grans (més de 500 pàgines). */
 
 declare(strict_types=1);
 
--- Testing/Nivel3/Ejercicio1/tests/unit/BibliotecaTest.php
+++ Testing/Nivel3/Ejercicio1/tests/unit/BibliotecaTest.php
@@ -1,9 +1,31 @@
 <?php
+/* Necessitem emmagatzemar el conjunt de llibres i tenir mètodes que:
+
+Afegeixin, esborrin i modifiquin un llibre de la llibreria.
+Permetin consultar llibres per títol, gènere, ISBN o autor.
+Retornar llibres grans (més de 500 pàgines). */
 
 declare(strict_types=1);
 
+use Ejercicio1\Biblioteca;
+use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
 
 final class BibliotecaTest extends TestCase
 {
+
+  #[Test]
+  public function añadir_un_libro()
+  {
+    $biblioteca = new Biblioteca();
+    $libro = [
+      'título' => 'El Quijote',
+      'autor' => 'Miguel de Cervantes',
+      'ISBN' => '123456789',
+      'género' => 'Romance',
+      'páginas' => 1000
+    ];
+    $agregarLibro = $biblioteca->agregar_libro();
+    $this->assertSame($libro, $agregarLibro, "El libro se ha añadido correctamente");
+  }
 }
