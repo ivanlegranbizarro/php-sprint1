027b9cbab91b6e8c060f1b7323bdb97fa50b8fe6
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-03-20 16:47:46 +0100

    eliminar y modificar libro

--- Testing/Nivel3/Ejercicio1/tests/unit/BibliotecaTest.php
+++ Testing/Nivel3/Ejercicio1/tests/unit/BibliotecaTest.php
@@ -19,6 +19,7 @@ final class BibliotecaTest extends TestCase
   {
     $biblioteca = new Biblioteca();
     $libro = [
+      'id' => 1,
       'título' => 'El Quijote',
       'autor' => 'Miguel de Cervantes',
       'ISBN' => '123456789',
@@ -27,5 +28,26 @@ final class BibliotecaTest extends TestCase
     ];
     $agregarLibro = $biblioteca->agregar_libro();
     $this->assertSame($libro, $agregarLibro, "El libro se ha añadido correctamente");
+  }
+
+  #[Test]
+  public function eliminar_un_libro()
+  {
+    $biblioteca = new Biblioteca();
+    $id_libro_a_eliminar = 1;
+    $libro_eliminado = $biblioteca->borrar_libro($id_libro_a_eliminar);
+    $this->assertSame($libro_eliminado, "El libro se ha eliminado correctamente");
+  }
+
+  #[Test]
+  public function modificar_un_libro()
+  {
+    $biblioteca = new Biblioteca();
+    $id_libro_a_modificar = 1;
+    $parametros_a_modificar = [
+      'título' => 'El Quijote nueva edición',
+    ];
+    $libro_modificado = $biblioteca->modificar_libro($id_libro_a_modificar, $parametros_a_modificar);
+    $this->assertSame($libro_modificado, "El libro se ha modificado correctamente");
   }
 }
