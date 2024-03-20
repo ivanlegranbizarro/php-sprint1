8a6508140d2ea2b0c252181352af83b0aa2eaac9
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-03-20 12:12:37 +0100

    setup Nivel3

---
+++ Testing/Nivel3/Ejercicio1/tests/unit/BibliotecaTest.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+use Ejercicio1\NumberChecker;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+
+final class BibliotecaTest extends TestCase
+{
+}
