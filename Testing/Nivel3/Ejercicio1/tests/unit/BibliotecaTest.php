4e25da820596e511009d4dc36b00fd3ac5669dca
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-03-20 16:56:06 +0100

    todos los tests

--- Testing/Nivel3/Ejercicio1/tests/unit/BibliotecaTest.php
+++ Testing/Nivel3/Ejercicio1/tests/unit/BibliotecaTest.php
@@ -50,4 +50,32 @@ final class BibliotecaTest extends TestCase
     $libro_modificado = $biblioteca->modificar_libro($id_libro_a_modificar, $parametros_a_modificar);
     $this->assertSame($libro_modificado, "El libro se ha modificado correctamente");
   }
+
+  #[Test]
+  public function buscar_libros()
+  {
+    $biblioteca = new Biblioteca();
+    $parametros_de_busqueda = [
+      'título' => 'El Quijote'
+    ];
+    $libros_encontrados = $biblioteca->consultar_libros($parametros_de_busqueda);
+
+    $this->assertNotEmpty($libros_encontrados);
+
+    $primer_libro = reset($libros_encontrados);
+    $this->assertSame(1, $primer_libro['id']);
+    $this->assertSame('El Quijote', $primer_libro['título']);
+    $this->assertSame('Miguel de Cervantes', $primer_libro['autor']);
+    $this->assertSame('123456789', $primer_libro['ISBN']);
+    $this->assertSame('Romance', $primer_libro['género']);
+    $this->assertSame(1000, $primer_libro['páginas']);
+  }
+
+  #[Test]
+  public function libros_grandes()
+  {
+    $biblioteca = new Biblioteca();
+    $libros_grandes = $biblioteca->libros_grandes();
+    $this->assertGreaterThan(500, $libros_grandes);
+  }
 }
