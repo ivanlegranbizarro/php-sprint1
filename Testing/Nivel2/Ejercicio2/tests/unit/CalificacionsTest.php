18d57d59124c3591d4cd60e2f25b2057fb4302b0
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-03-20 12:02:51 +0100

    Nivel2 Ejercicio2

--- Testing/Nivel2/Ejercicio2/tests/unit/CalificacionsTest.php
+++ Testing/Nivel2/Ejercicio2/tests/unit/CalificacionsTest.php
@@ -3,41 +3,28 @@
 declare(strict_types=1);
 
 use Ejercicio2\Calificacions;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
 
 final class CalificacionsTest extends TestCase
 {
 
-  #[Test]
-  public function si_la_nota_es_inferior_a_33_reprova(): void
+  public static function notas_a_probar(): array
   {
-    $calificacio = new Calificacions(32);
-    $resultat = $calificacio->verificarGrado();
-    $this->assertEquals($resultat, 'Reprova');
+    return [
+      [32, 'Reprova'],
+      [33, 'Tercera Divisió'],
+      [46, 'Segona Divisió'],
+      [60, 'Primera Divisió']
+    ];
   }
 
   #[Test]
-  public function si_la_nota_esta_entre_33_i_45_tercera_divisió(): void
+  #[DataProvider('notas_a_probar')]
+  public function calificacions_per_nota(int $nota, string $resultat): void
   {
-    $calificacio = new Calificacions(33);
-    $resultat = $calificacio->verificarGrado();
-    $this->assertEquals($resultat, 'Tercera Divisió');
-  }
-
-  #[Test]
-  public function si_la_nota_esta_entre_45_i_60_segona_divisió(): void
-  {
-    $calificacio = new Calificacions(46);
-    $resultat = $calificacio->verificarGrado();
-    $this->assertEquals($resultat, 'Segona Divisió');
-  }
-
-  #[Test]
-  public function si_la_nota_es_superior_a_60_primera_divisió(): void
-  {
-    $calificacio = new Calificacions(60);
-    $resultat = $calificacio->verificarGrado();
-    $this->assertEquals($resultat, 'Primera Divisió');
+    $calificacio = new Calificacions($nota);
+    $this->assertSame($resultat, $calificacio->verificarGrado());
   }
 }
