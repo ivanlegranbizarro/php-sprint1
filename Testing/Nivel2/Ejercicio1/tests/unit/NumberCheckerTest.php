e0d8a04d8b57ffec5e9da9b32ca0e2b946020082
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-03-20 11:49:42 +0100

    Nivel2 Ejercicio1

--- Testing/Nivel2/Ejercicio1/tests/unit/NumberCheckerTest.php
+++ Testing/Nivel2/Ejercicio1/tests/unit/NumberCheckerTest.php
@@ -3,36 +3,42 @@
 declare(strict_types=1);
 
 use Ejercicio1\NumberChecker;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
 
 final class NumberCheckerTest extends TestCase
 {
-  #[Test]
-  public function probar_pares(): void
+  public static function numeros_a_probar_pares_impares(): array
   {
-    $prueba = new NumberChecker(2);
-    $this->assertTrue($prueba->isEven());
+    return [
+      [2, true],
+      [3, false]
+    ];
+  }
+
+  public static function numeros_a_probar_positivos_negativos(): array
+  {
+    return [
+      [2, true],
+      [-5, false]
+    ];
+  }
+
+
+  #[Test]
+  #[DataProvider('numeros_a_probar_pares_impares')]
+  public function probar_pares_impares(int $numero, bool $esPar): void
+  {
+    $prueba = new NumberChecker($numero);
+    $this->assertSame($esPar, $prueba->isEven());
   }
 
   #[Test]
-  public function probar_impares(): void
+  #[DataProvider('numeros_a_probar_positivos_negativos')]
+  public function probar_si_es_positivo($numero, $esPositivo): void
   {
-    $prueba = new NumberChecker(3);
-    $this->assertFalse($prueba->isEven());
-  }
-
-  #[Test]
-  public function probar_si_es_positivo(): void
-  {
-    $prueba = new NumberChecker(2);
-    $this->assertTrue($prueba->isPositive());
-  }
-
-  #[Test]
-  public function probar_si_es_negativo(): void
-  {
-    $prueba = new NumberChecker(-5);
-    $this->assertFalse($prueba->isPositive());
+    $prueba = new NumberChecker($numero);
+    $this->assertSame($esPositivo, $prueba->isPositive());
   }
 }
